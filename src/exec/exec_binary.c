f539971b4cb9dea5f13fb339a8348ca41fcf9655
Wati-Theo <76228129+Wati-Theo@users.noreply.github.com>
2022-10-05 01:09:24 +0200

    relative path exec ok

--- src/exec/exec_binary.c
+++ src/exec/exec_binary.c
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 01:08:33 by Wati-Theo         #+#    #+#             */
-/*   Updated: 2022/10/03 23:53:30 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2022/10/05 01:07:31 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,25 @@ char	*get_path(t_global *mini_sh)
 int	is_relative(t_global *mini_sh, t_lst_cmd *cmd)
 {
 	if ((cmd->exec[0][0] == '.' || cmd->exec[0][0] == '/'))
-		return (1);
+	{
+		if (access(cmd->exec[0], X_OK) == 0)
+			return (1);
+		else if (access(cmd->exec[0], F_OK) == 0)
+		{
+			ft_putstr_fd("wati-minishell: ", 2);
+			ft_putstr_fd(cmd->exec[0], 2);
+			ft_putendl_fd(": Permission denied", 2);
+			mini_sh->ret = 126;
+			exit(126);
+		}
+		else
+		{
+			ft_putendl_fd(ft_strjoin(ft_strjoin("wati-minishell: ", cmd->exec[0]\
+			, mini_sh), ": No such file or directory", mini_sh), 2);
+			mini_sh->ret = 127;
+			exit(127);
+		}
+	}
 	return (0);
 }
 
@@ -46,11 +64,6 @@ char	*get_binary(t_global *mini_sh, t_lst_cmd *cmd)
 	i = 0;
 	while (path_splited[i])
 	{
-		// bin = ft_calloc(sizeof(char), ft_strlen(path_splited[i]) + ft_strlen(cmd->exec[0]) + 2);
-		// ft_strjoin(ft_strjoin(path_splited[i], "/", mini_sh), cmd->exec[0], mini_sh);
-		// ft_strlcat(bin, path_splited[i], -1);
-		// ft_strlcat(bin, "/", -1);
-		// ft_strlcat(bin, cmd->exec[0], -1);
 		if (access(ft_strjoin(ft_strjoin(path_splited[i], "/", mini_sh), cmd->exec[0], mini_sh), F_OK) == 0)
 			return (ft_strjoin(ft_strjoin(path_splited[i], "/", mini_sh), cmd->exec[0], mini_sh));
 		i++;
